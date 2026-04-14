a704c436a323aa3969e016cea303def4cef43f21
galaxyA15hype <wiem.mkacher20@gmail.com>
2026-04-14 18:50:01 +0100

    Update ksu to 3.2.4

--- build_kernel.sh
+++ build_kernel.sh
@@ -91,7 +91,7 @@ cd kernel-5.10
 
 print_msg "$GREEN" "Setting up KernelSU..."
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s 3.2.2
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s 3.2.4
 
 print_msg "$GREEN" "Finished Setting up KernelSU..."
 
